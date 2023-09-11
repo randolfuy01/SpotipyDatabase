73f8e533903dca20baae448c6abcee64f3bf5f3e
Randolf Uy <143443631+randolfuy01@users.noreply.github.com>
2023-09-11 08:20:23 -0700

    Added token authorization and headers

--- SpotifyAPI.py
+++ SpotifyAPI.py
@@ -1,1 +1,23 @@
+import requests
+class SpotifyAPI:
+    
+    def __init__(self) -> None:
+        self.clientID = ""
+        self.clientSecret = ""
 
+    def getToken(self):
+        authUrl = 'https://accounts.spotify.com.api/token'
+
+        authResponse = requests.post(authUrl, {
+            'grant_type': 'client_credentials',
+            'client_id' : self.clientID,
+            'client_secret' : self.clientSecret
+        })
+
+        authResponseData = authResponse.json()
+        accessToken = authResponseData['access_token']
+        return accessToken
+    
+    def getHeader(self):
+        headers = {'Authorization': 'Bearer {token}'.format(token = self.getToken())}
+        return headers
